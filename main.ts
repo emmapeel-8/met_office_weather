d582db7b7b47bb24a30ffcd4a2211719b0e3b28f
Emma Peel <Emma.Peel@softwire.com>
2026-09-14 15:24:02 +0100

    Add postcode lookup instead of manual latitude and logitude

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 import "dotenv/config";
 import { createInterface } from "node:readline/promises";
-import { fetchWeatherThreeHours } from "./api/metOffice.ts";
+import { fetchWeatherThreeHours} from "./api/metOffice.ts";
+import { fetchPostcode} from "./api/postcodes.ts";
 import type { WeatherResponse } from "./types/weather_types.ts";
+import type { PostcodeInfo } from "./types/postcode_types.ts";
 
 async function promptNumber(rl: ReturnType<typeof createInterface>, question: string): Promise<number> {
     while (true) {
@@ -33,12 +35,15 @@ async function main() {
     const rl = createInterface({ input: process.stdin, output: process.stdout });
 
     try {
-        const latitude = await promptNumber(rl, "Latitude: ");
-        const longitude = await promptNumber(rl, "Longitude: ");
+        //const latitude = await promptNumber(rl, "Latitude: ");
+        //const longitude = await promptNumber(rl, "Longitude: ");
+        const postcode : string = await rl.question("Enter postcode: ");
 
-        const data: WeatherResponse[] = await fetchWeatherThreeHours(latitude, longitude);
-        const nextThreeHours = getNextThreeHours(data);
+        const postcode_data: PostcodeInfo = await fetchPostcode(postcode)
+        const weather: WeatherResponse[] = await fetchWeatherThreeHours(postcode_data.latitude, postcode_data.longitude);
+        const nextThreeHours: WeatherResponse[] = getNextThreeHours(weather);
 
+        console.log(`Weather report for ${postcode_data.parliamentary_constituency}:`)
         for (const entry of nextThreeHours) {
             console.log(formatWeather(entry));
         }
